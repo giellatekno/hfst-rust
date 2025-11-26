52f46e60c03e368798942fe140fbb2bcc6457276
Anders Lorentsen <anders.lorentsen@gmail.com>
2025-11-26 14:14:04 +0100

    feat: implement tokio actor that does lookups

--- hfst/src/lib.rs
+++ hfst/src/lib.rs
@@ -1,13 +1,16 @@
-//! Rust bindings to [libhfst](https://hfst.github.io/) (*)
+#![cfg_attr(doc, feature(doc_cfg))]
+
+//! Rust bindings to _some of_ [libhfst](https://hfst.github.io/) (*)
 //!
-//! This library is ergonomic wrappers around `hfst_sys`.
-//!
-//! (*) Some (_most_) functionality missing.
+//! This library is ergonomic wrappers around [hfst_sys](https://docs.rs/hfst-sys).
+
+#[cfg(feature = "tokio-actors")]
+pub mod transducer_actor;
 
 use hfst_sys;
-use std::ffi::{c_float, CString};
-use std::path::Path;
+use std::ffi::{CString, c_float};
 use std::os::raw::{c_char, c_void};
+use std::path::Path;
 use std::ptr::addr_of_mut;
 
 fn strlen(s: *const c_char) -> usize {
@@ -24,13 +27,13 @@ fn strlen(s: *const c_char) -> usize {
 /// and appending a null byte at the end.
 fn str_to_boxed_c_charptr(s: &str) -> Box<[c_char]> {
     let v = Vec::from_iter(
-        s
-            .as_bytes()
+        s.as_bytes()
             .iter()
             .copied()
             .map(|b| b as c_char)
-            .chain(std::iter::once(0 as c_char))
-    ).into_boxed_slice();
+            .chain(std::iter::once(0 as c_char)),
+    )
+    .into_boxed_slice();
     assert_eq!(s.len(), strlen(v.as_ptr()));
     v
 }
@@ -56,34 +59,42 @@ pub struct HfstTransducer {
 /// SAFETY: The transducer can move between threads. Nothing will go wrong
 /// if one thread creates an HfstTransducer, and then another thread uses it.
 /// (This is required in order to be able to store in a Axum's State)
+///
+/// But, NOTE: HfstTransducer is *NOT* thread-safe: Two diffferent threads that holds
+/// a reference to it, can *not* call .e.g `lookup()` at the same time (it SIGSEGVs).
 unsafe impl Send for HfstTransducer {}
+// WOULD NOT WORK:
+//unsafe impl Sync for HfstTransducer {}
 
-// anders: the code currently in hfst doesn't check errors.
-// /// Errors related to HfstInputStreams.
-// #[derive(Debug)]
-// pub enum HfstInputStreamError {
-//     /// File not found, cannot be opened, or libhfst doesn't think this file
-//     /// is an hfst file. This variant corresponds to the
-//     /// NotTransducerStreamException in the C++ API.
-//     NotTransducerStream,
-//     /// Stream is at End Of File. This variant corresponds to
-//     /// `HfstInputStream::is_eof()` in the C++ API.
-//     Eof,
-//     /// Bad stream. Essentially a stream is Bad if any OS-level IO errors has
-//     /// occurred. This variant corresponds to `HfstInputStream::is_bad()` in
-//     /// the C++ API.
-//     Bad,
-//     /// The stream is recognized as a type of FST, but the version of libhfst
-//     /// that is in use, has not been compiled with support for this type of
-//     /// fst. This variant corresponds to
-//     /// `ImplementationTypeNotAvailableException` in the C++ API.
-//     ImplementationTypeNotAvailable,
-// }
-// */
+/// Errors related to HfstInputStreams.
+#[derive(Debug, thiserror::Error)]
+pub enum HfstInputStreamError {
+    /// File not found, cannot be opened, or libhfst doesn't think this file
+    /// is an hfst file. This variant corresponds to the
+    /// NotTransducerStreamException in the C++ API.
+    #[error("Not a transducer stream")]
+    NotTransducerStream,
+    /// Stream is at End Of File. This variant corresponds to
+    /// `HfstInputStream::is_eof()` in the C++ API.
+    #[error("input stream at EOF")]
+    Eof,
+    /// Bad stream. Essentially a stream is Bad if any OS-level IO errors has
+    /// occurred. This variant corresponds to `HfstInputStream::is_bad()` in
+    /// the C++ API.
+    #[error("Bad input stream")]
+    Bad,
+    /// The stream is recognized as a type of FST, but the version of libhfst
+    /// that is in use, has not been compiled with support for this type of
+    /// fst. This variant corresponds to
+    /// `ImplementationTypeNotAvailableException` in the C++ API.
+    #[error("Implementation type not available")]
+    ImplementationTypeNotAvailable,
+}
 
 impl HfstInputStream {
     /// Load a file as an HfstInputStream.
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, ()> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, HfstInputStreamError> {
+        use HfstInputStreamError as Error;
         // this is apparently wrong and/or suboptimal, but going from
         // a Path to a C char* is apparently not straight forward
         let path = CString::new(format!("{}", path.as_ref().display())).unwrap();
@@ -95,17 +106,17 @@ impl HfstInputStream {
             if !stream.is_null() {
                 Ok(Self { inner: stream })
             } else {
-                return Err(());
-                // anders: will be used when libhfst gives an error code
-                // on stream reading failure
-                //use HfstInputStreamError::*;
+                // TODO: Use better error handling. Probably this will be sending a
+                // pointer to an int to hfst_input_stream(), where it can write the
+                // error code.
+                Err(Error::NotTransducerStream)
                 //match err as u32 {
                 //    hfst_sys::NOT_TRANSDUCER_STREAM => {
-                //        Err(NotTransducerStream)
+                //        Err(Error::NotTransducerStream)
                 //    }
-                //    hfst_sys::END_OF_STREAM => { Err(Eof) }
+                //    hfst_sys::END_OF_STREAM => { Err(Error::Eof) }
                 //    hfst_sys::IMPLEMENTATION_TYPE_NOT_AVAILABLE => {
-                //        Err(ImplementationTypeNotAvailable)
+                //        Err(Error::ImplementationTypeNotAvailable)
                 //    }
                 //    _ => unreachable!("all possible err values covered")
                 //}
@@ -114,25 +125,48 @@ impl HfstInputStream {
     }
 
     /// Read the transducers from this HfstInputStream.
-    pub fn read_transducers(&self) -> Vec<HfstTransducer> {
-        let handle = self.inner;
-        let mut transducers = vec![];
-        loop {
-            let bad = unsafe { hfst_sys::hfst_input_stream_is_bad(handle) };
-            if bad {
-                break;
-            }
-            let eof = unsafe { hfst_sys::hfst_input_stream_is_eof(handle) };
-            if eof {
-                break;
+    pub fn read_transducers(&self) -> impl Iterator<Item = HfstTransducer> {
+        std::iter::from_fn(|| {
+            if unsafe { hfst_sys::hfst_input_stream_is_bad(self.inner) } {
+                return None;
+            } else if unsafe { hfst_sys::hfst_input_stream_is_eof(self.inner) } {
+                return None;
             }
             let tr = unsafe { hfst_sys::hfst_transducer_from_stream(self.inner) };
             if tr.is_null() {
-                continue;
+                return None;
             }
-            transducers.push(HfstTransducer { inner: tr });
+            return Some(HfstTransducer { inner: tr });
+        })
+        //let mut transducers = vec![];
+        //loop {
+        //    let bad = unsafe { hfst_sys::hfst_input_stream_is_bad(handle) };
+        //    if bad {
+        //        break;
+        //    }
+        //    let eof = unsafe { hfst_sys::hfst_input_stream_is_eof(handle) };
+        //    if eof {
+        //        break;
+        //    }
+        //    let tr = unsafe { hfst_sys::hfst_transducer_from_stream(self.inner) };
+        //    if tr.is_null() {
+        //        continue;
+        //    }
+        //    transducers.push(HfstTransducer { inner: tr });
+        //}
+        //transducers
+    }
+
+    /// Return the *one* transducer that exists in this `HfstInputStream` as
+    /// [`Some(transducer)`], or return [`None`] if there are no transducers, or
+    /// more than one.
+    pub fn read_only_transducer(&self) -> Option<HfstTransducer> {
+        let mut it = self.read_transducers();
+        let transducer = it.next();
+        if let Some(_) = it.next() {
+            return None;
         }
-        transducers
+        transducer
     }
 }
 
@@ -235,9 +269,18 @@ mod tests {
         let query = "sko";
         let results = transducer.lookup(query);
         let mut seen = std::collections::HashMap::new();
-        seen.insert("sko+N+Msc+Pl+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@", false);
-        seen.insert("sko+N+Msc+Pl+Nynorsk+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@", false);
-        seen.insert("sko+N+Msc+Sg+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@", false);
+        seen.insert(
+            "sko+N+Msc+Pl+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@",
+            false,
+        );
+        seen.insert(
+            "sko+N+Msc+Pl+Nynorsk+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@",
+            false,
+        );
+        seen.insert(
+            "sko+N+Msc+Sg+Indef@D.CmpOnly.FALSE@@D.CmpPref.TRUE@@D.NeedNoun.ON@",
+            false,
+        );
         seen.insert("sko+V+Imp", false);
         seen.insert("sko+V+Inf", false);
 
@@ -247,4 +290,45 @@ mod tests {
 
         assert!(seen.into_iter().all(|(_k, v)| v));
     }
+
+    // NOTE: This was a test meant to test that HfstTransducer::lookup worked correctly
+    // when called from multiple threads. It does not. It segfaults (SIGSEGV). This is
+    // expected, as the underlying C++ HfstTransducer is not thread-safe.
+    //
+    //#[test]
+    //fn can_lookup_from_multiple_threads() {
+    //    use std::sync::Arc;
+
+    //    let input_stream = HfstInputStream::new(PATH).unwrap();
+    //    let mut transducers = input_stream.read_transducers();
+    //    let transducer = transducers.pop().expect("exactly 1 transducer");
+    //    assert!(transducers.is_empty(), "only 1 transducer");
+
+    //    let transducer = Arc::new(transducer);
+
+    //    let mut threads = vec![];
+    //    for _ in 0..1 {
+    //        // Error:
+    //        // rustc: `*mut c_void` cannot be shared between threads safely
+    //        // within `HfstTransducer`, the trait `Sync` is not implemented for `*mut c_void`
+    //        // required for `Arc<HfstTransducer>` to implement `Send`
+    //        //
+    //        // And we *can not* implement Sync for HfstTransducer, see safety comment
+    //        // on the impl Send for HfstTransducer line.
+    //        let jh = std::thread::spawn({
+    //            let transducer = Arc::clone(&transducer);
+    //            move || {
+    //                let lookup = transducer.lookup("viessu");
+    //                for (s, _w) in lookup {
+    //                    println!("{s}");
+    //                }
+    //            }
+    //        });
+    //        threads.push(jh);
+    //    }
+    //
+    //    for thread in threads {
+    //        thread.join().expect("thread didn't panic");
+    //    }
+    //}
 }
---
+++ hfst/src/transducer_actor.rs
@@ -0,0 +1,411 @@
+//! A [tokio](https://docs.rs/tokio) *actor* for doing lookups from multiple tokio tasks.
+//!
+//! The main [`crate::HfstTransducer`] is *not* thread-safe, and only supports
+//! doing one lookup at a time. This module defines a tokio *actor* that tasks
+//! can send lookup requests to in parallel, and return back results.
+//!
+//! Lookup request messages are sent to the actor, and the actor simply runs an
+//! infinite loop where it pulls off lookup requests, one by one. It does the
+//! lookup, and sends back the replies in a *oneshot* channel.
+//!
+//! # Example
+//! ```rust
+//! use std::sync::Arc;
+//! use hfst::transducer_actor::{LookupResult, HfstTransducerActor};
+//!
+//! // First, build yourself a Transducer
+//! let transducer = /* some transducer */();
+//!
+//! // Then, build an actor that takes ownership of this transducer.
+//! let actor = HfstTransducerActor::builder()
+//!     .transducer(transducer)
+//!     .queue_size(std::num::NonZeroUsize::new(100).unwrap())
+//!     .timings(true)
+//!     .build();
+//!
+//! // Put the actor in an Arc, so it can be shared
+//! let actor = Arc::new(actor);
+//!
+//! let tasks: Vec<_> = (0..9).map(|_| tokio::task::spawn({
+//!     // Get a new reference to the actor.
+//!     let actor = Arc::clone(&actor);
+//!     // ...The new reference is moved in.
+//!     async move {
+//!         let lookup = actor.lookup("viessu").await.expect("lookup did not error");
+//!         let LookupResults { results, .. } = lookup;
+//!         println!("{}", results.join("\n"));
+//!     }
+//! })).collect();
+//!
+//! for task in tasks {
+//!     task.await.expect("task did not panic");
+//! }
+//! ```
+
+use std::sync::{Arc, Mutex};
+use std::time::{Duration, Instant};
+
+use tokio::sync::{mpsc, oneshot};
+
+use crate::{HfstInputStream, HfstTransducer};
+
+/// A running HfstTransducer actor.
+pub struct HfstTransducerActor {
+    jh: tokio::task::JoinHandle<HfstTransducer>,
+    tx: mpsc::Sender<LookupMessage>,
+}
+
+/// The result we get back from `HfstTransducerActor::lookup()`.
+pub struct LookupResults {
+    /// The actual results: The string, and the weight.
+    pub results: Vec<(String, f32)>,
+
+    /// We did wait before we entered the queue, and if so, for how long?
+    pub before_queue: Waited,
+
+    /// Did we wait *in* the queue, and if so, for how long?
+    pub in_queue: Waited,
+
+    /// How long the actual lookup took
+    pub lookup_duration: Duration,
+
+    /// How long it took before the result came back
+    pub result_duration: Duration,
+}
+
+#[derive(Debug)]
+pub enum LookupError {
+    ChannelClosed,
+}
+
+/// Did we wait? If so, for how long?
+pub enum Waited {
+    Yes(Duration),
+    No,
+}
+
+/// Message that is sent to the lookup actor from the many clients.
+enum LookupMessage {
+    Lookup(String, oneshot::Sender<LookupReply>),
+
+    /// Message to quit the actor
+    Quit,
+}
+
+/// Internal Reply message that is sent back from the actor to `HfstTransducerActor::lookup()`
+#[derive(Debug)]
+struct LookupReply {
+    results: Vec<(String, f32)>,
+    lookup_duration: Duration,
+}
+
+mod builder {
+    use super::HfstTransducerActor;
+    use crate::HfstTransducer;
+    use std::num::NonZeroUsize;
+
+    /// The builder for [`HfstTransducerActor`]. It takes three values:
+    /// - **transducer** (*required*). An [`crate::HfstTransducer`]. The transducer to use.
+    /// - **queue_size** (*required*) A [`std::num::NonZeroUsize`]. The size of the tokio mpsc queue.
+    /// - **timings** (*optional*), a [`bool`]. Whether or not to return timings in lookups.
+    ///
+    /// ## Example
+    /// ```
+    /// let actor = HfstTransducerActor::builder()
+    ///     .transducer(/* transducer */)
+    ///     .queue_size(std::num::NonZeroUsize::new(100).unwrap())
+    ///     .timings(true)
+    ///     .build();
+    /// ```
+    pub struct Builder<A, B, C> {
+        transducer: A,
+        queue_size: B,
+        timings: C,
+    }
+
+    // Beware: Custom implemented type state pattern builder below...
+
+    pub struct TransducerEmpty;
+    pub struct TransducerAdded(HfstTransducer);
+    pub struct QueueSizeEmpty;
+    pub struct QueueSizeAdded(NonZeroUsize);
+    pub struct TimingsEmpty;
+    pub struct TimingsAdded(bool);
+
+    pub type EmptyBuilder = Builder<TransducerEmpty, QueueSizeEmpty, TimingsEmpty>;
+
+    impl Default for Builder<TransducerEmpty, QueueSizeEmpty, TimingsEmpty> {
+        fn default() -> Self {
+            Self {
+                transducer: TransducerEmpty,
+                queue_size: QueueSizeEmpty,
+                timings: TimingsEmpty,
+            }
+        }
+    }
+
+    #[doc(hidden)]
+    impl Builder<TransducerEmpty, QueueSizeEmpty, TimingsEmpty> {
+        pub fn transducer(
+            self,
+            tr: HfstTransducer,
+        ) -> Builder<TransducerAdded, QueueSizeEmpty, TimingsEmpty> {
+            Builder {
+                transducer: TransducerAdded(tr),
+                queue_size: QueueSizeEmpty,
+                timings: TimingsEmpty,
+            }
+        }
+
+        pub fn queue_size(
+            self,
+            size: NonZeroUsize,
+        ) -> Builder<TransducerEmpty, QueueSizeAdded, TimingsEmpty> {
+            Builder {
+                transducer: TransducerEmpty,
+                queue_size: QueueSizeAdded(size),
+                timings: TimingsEmpty,
+            }
+        }
+
+        pub fn timings(
+            self,
+            enabled: bool,
+        ) -> Builder<TransducerEmpty, QueueSizeEmpty, TimingsAdded> {
+            Builder {
+                transducer: TransducerEmpty,
+                queue_size: QueueSizeEmpty,
+                timings: TimingsAdded(enabled),
+            }
+        }
+    }
+
+    // === (1, 0, 0)
+    #[doc(hidden)]
+    impl Builder<TransducerAdded, QueueSizeEmpty, TimingsEmpty> {
+        pub fn queue_size(
+            self,
+            size: NonZeroUsize,
+        ) -> Builder<TransducerAdded, QueueSizeAdded, TimingsEmpty> {
+            Builder {
+                transducer: self.transducer,
+                queue_size: QueueSizeAdded(size),
+                timings: TimingsEmpty,
+            }
+        }
+
+        pub fn timings(
+            self,
+            enabled: bool,
+        ) -> Builder<TransducerAdded, QueueSizeEmpty, TimingsAdded> {
+            Builder {
+                transducer: self.transducer,
+                queue_size: QueueSizeEmpty,
+                timings: TimingsAdded(enabled),
+            }
+        }
+    }
+
+    // === (0, 1, 0)
+    #[doc(hidden)]
+    impl Builder<TransducerEmpty, QueueSizeAdded, TimingsEmpty> {
+        pub fn transducer(
+            self,
+            tr: HfstTransducer,
+        ) -> Builder<TransducerAdded, QueueSizeAdded, TimingsEmpty> {
+            Builder {
+                transducer: TransducerAdded(tr),
+                queue_size: self.queue_size,
+                timings: TimingsEmpty,
+            }
+        }
+
+        pub fn timings(
+            self,
+            enabled: bool,
+        ) -> Builder<TransducerEmpty, QueueSizeAdded, TimingsAdded> {
+            Builder {
+                transducer: TransducerEmpty,
+                queue_size: self.queue_size,
+                timings: TimingsAdded(enabled),
+            }
+        }
+    }
+
+    // === (0, 0, 1)
+    #[doc(hidden)]
+    impl Builder<TransducerEmpty, QueueSizeEmpty, TimingsAdded> {
+        pub fn transducer(
+            self,
+            tr: HfstTransducer,
+        ) -> Builder<TransducerAdded, QueueSizeEmpty, TimingsAdded> {
+            Builder {
+                transducer: TransducerAdded(tr),
+                queue_size: QueueSizeEmpty,
+                timings: self.timings,
+            }
+        }
+
+        pub fn queue_size(
+            self,
+            size: NonZeroUsize,
+        ) -> Builder<TransducerEmpty, QueueSizeAdded, TimingsAdded> {
+            Builder {
+                transducer: TransducerEmpty,
+                queue_size: QueueSizeAdded(size),
+                timings: self.timings,
+            }
+        }
+    }
+
+    #[doc(hidden)]
+    impl Builder<TransducerAdded, QueueSizeAdded, TimingsEmpty> {
+        pub fn timings(
+            self,
+            enabled: bool,
+        ) -> Builder<TransducerAdded, QueueSizeAdded, TimingsAdded> {
+            Builder {
+                transducer: self.transducer,
+                queue_size: self.queue_size,
+                timings: TimingsAdded(enabled),
+            }
+        }
+
+        pub fn build(self) -> HfstTransducerActor {
+            let transducer = self.transducer.0;
+            let queue_size = self.queue_size.0.get();
+            HfstTransducerActor::new(transducer, queue_size)
+        }
+    }
+
+    #[doc(hidden)]
+    impl Builder<TransducerAdded, QueueSizeAdded, TimingsAdded> {
+        pub fn build(self) -> HfstTransducerActor {
+            let transducer = self.transducer.0;
+            let queue_size = self.queue_size.0.get();
+            HfstTransducerActor::new(transducer, queue_size)
+        }
+    }
+}
+
+impl HfstTransducerActor {
+    /// Create a new `HfstTransducerActor` through this easy-to-use [`builder::Builder`].
+    pub fn builder() -> builder::EmptyBuilder {
+        builder::Builder::default()
+    }
+
+    fn new(transducer: HfstTransducer, queue_size: usize) -> HfstTransducerActor {
+        let (tx, mut rx) = mpsc::channel(queue_size);
+
+        let jh = tokio::task::spawn(async move {
+            while let Some(msg) = rx.recv().await {
+                match msg {
+                    LookupMessage::Lookup(input, result_tx) => {
+                        let t0 = Instant::now();
+                        let results: Vec<_> = transducer.lookup(&input).into_iter().collect();
+                        let lookup_duration = t0.elapsed();
+                        let reply_message = LookupReply {
+                            results,
+                            lookup_duration,
+                        };
+                        result_tx
+                            .send(reply_message)
+                            .expect("reciever didn't hang up");
+                    }
+                    LookupMessage::Quit => break,
+                }
+            }
+            transducer
+        });
+
+        HfstTransducerActor { jh, tx }
+    }
+
+    /// Look up a value in the transducer.
+    ///
+    /// ```
+    /// use std::sync::Arc;
+    /// use hfst::transducer_actor::LookupResults;
+    ///
+    /// let actor = /* ... */();
+    /// actor.lookup("input").await.unwrap();
+    /// let mut join_handles = vec![];
+    /// for _ in 0..10 {
+    ///     join_handles.push(tokio::task::spawn({
+    ///         let actor = Arc::clone(&actor);
+    ///         async move {
+    ///             let LookupResults { results, .. } = actor.lookup("input").await;
+    ///             results.for_each(|result| println!("{result}"));
+    ///         }
+    ///     }));
+    /// }
+    /// for join_handle in join_handles {
+    ///     join_handle.await.expect("task did not panic");
+    /// }
+    /// ```
+    pub async fn lookup(&self, input: &str) -> Result<LookupResults, LookupError> {
+        if self.tx.is_closed() {
+            return Err(LookupError::ChannelClosed);
+        }
+
+        let tx = self.tx.clone();
+        let (os_tx, os_rx) = oneshot::channel();
+        let message = LookupMessage::Lookup(input.into(), os_tx);
+        let before_queue = match tx.try_send(message) {
+            Ok(()) => Waited::No,
+            Err(mpsc::error::TrySendError::Closed(_message)) => {
+                return Err(LookupError::ChannelClosed);
+            }
+            Err(mpsc::error::TrySendError::Full(message)) => {
+                let t0 = Instant::now();
+                match tx.reserve().await {
+                    Ok(permit) => {
+                        let before_queue = Waited::Yes(t0.elapsed());
+                        permit.send(message);
+                        before_queue
+                    }
+                    Err(_) => {
+                        return Err(LookupError::ChannelClosed);
+                    }
+                }
+            }
+        };
+
+        // Message has been sent here into the queue here. We don't know at what position
+        // in the queue it entered into, or if there even was a queue at all.
+        let t0 = Instant::now();
+        let lookup_reply = os_rx.await.expect("channel was not closed in transit");
+        let result_duration = t0.elapsed();
+
+        let LookupReply {
+            results,
+            lookup_duration,
+        } = lookup_reply;
+
+        // Here we have to calculate a bit.
+        // We have result duration, which is the entire time from when the message
+        // was accepted into the queue, and we also have the actual time it took to look
+        // up the value, from the actor, so, we can calculate how long we waited in
+        // the queue.
+        let in_queue = Waited::Yes(result_duration - lookup_duration);
+
+        Ok(LookupResults {
+            results,
+            before_queue,
+            in_queue,
+            result_duration,
+            lookup_duration,
+        })
+    }
+
+    /// Stop the actor. Returns the ownership of the underlying [`HfstTransducer`] back
+    /// the caller.
+    pub async fn stop(self) -> HfstTransducer {
+        let HfstTransducerActor { tx, jh } = self;
+        let transducer = jh.await.expect("actor did not panic");
+        tx.send(LookupMessage::Quit)
+            .await
+            .expect("channel was not already closed");
+        transducer
+    }
+}
